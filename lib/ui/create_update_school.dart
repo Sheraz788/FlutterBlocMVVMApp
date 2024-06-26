ae12d4afe755ae36ad4f52f23b2b33d9355dbe16
Sheraz Ali <sherazalibk@gmail.com>
2024-06-26 16:54:54 +0500

    working on form

--- lib/ui/create_update_school.dart
+++ lib/ui/create_update_school.dart
@@ -8,6 +8,7 @@ class CreateOrUpdateSchool extends StatefulWidget {
 }
 
 class _CreateOrUpdateSchoolState extends State<CreateOrUpdateSchool> {
+
   final TextEditingController schoolNameCtrl = TextEditingController();
 
   final TextEditingController locationCtrl = TextEditingController();
@@ -24,20 +25,44 @@ class _CreateOrUpdateSchoolState extends State<CreateOrUpdateSchool> {
 
   final GlobalKey<FormState> formKey = GlobalKey<FormState>();
 
-  String? selectedCountry;
+  String? selectedCountry = countries[0];
 
   @override
   Widget build(BuildContext context) {
     return Column(
       mainAxisSize: MainAxisSize.min,
-      children: [Text("Create School")],
+      children: [
+        const Text("Create School"),
+        const Divider(),
+        _buildForm(),
+        const Divider()
+      ],
     );
   }
 
   Widget _buildForm() {
     return Form(
+      key: formKey,
+        autovalidateMode: AutovalidateMode.onUserInteraction,
         child: Padding(
-            padding: EdgeInsets.all(8)
+            padding: EdgeInsets.all(8),
+          child: Wrap(
+            spacing: 10,
+            runSpacing: 20,
+            children: [
+              TextFormField(controller: schoolNameCtrl, decoration: const InputDecoration(label: Text("School Name"), suffixIcon: Icon(Icons.school), border: OutlineInputBorder())),
+              
+              DropdownButtonFormField(
+                  items: countries.map((e) => DropdownMenuItem(child: Text(e), value: e,)).toList(),
+                  onChanged: (val){
+                    selectedCountry = val;
+                  },
+                value: selectedCountry,
+                style: const TextStyle(fontWeight: FontWeight.w500, color: Colors.black),
+                decoration: const InputDecoration(border: OutlineInputBorder()),
+              )
+            ],
+          ),
         )
     );
   }
